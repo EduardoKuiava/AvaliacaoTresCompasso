c21a49569aa845bd371f71fd3905bbb5c4f5a09d
EduardoKuiava <75549530+EduardoKuiava@users.noreply.github.com>
2021-10-24 23:39:58 -0300

    Delete avaliacao directory

--- avaliacao/target/classes/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO ESTADO(nome, regiao, populacao, capital, area) VALUES('Rio Grande Do Norte', 'NORDESTE', '3409001', 'Natal', '52.300');
-INSERT INTO ESTADO(nome, regiao, populacao, capital, area) VALUES('Rio Grande Do Sul', 'SUL', '3409002', 'Porto Alegre', '52.200');
-INSERT INTO ESTADO(nome, regiao, populacao, capital, area) VALUES('São Paulo', 'CENTRO_OESTE', '3409003', 'São Paulo', '52.100');
